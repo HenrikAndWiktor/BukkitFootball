04fe95c93c38e7e20bc0e3fcd1d6f2fff513b781
HenrikAndWiktor <wiktor.istok.eriksson@gmail.com>
2017-07-31 15:13:37 +0200

    Added javadocs

--- src/se/wiktoreriksson/futbol/FootballMain.java
+++ src/se/wiktoreriksson/futbol/FootballMain.java
@@ -9,23 +9,59 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- * Created by wiktoreriksson on 2017-07-28.
+ * Created by Wiktor Eriksson on 2017-07-28.
+ * @author Wiktor Eriksson
+ * @see org.bukkit.plugin.java.JavaPlugin
+ * @since 1.0.1
  */
 public class FootballMain extends JavaPlugin{
+    /**
+     * The enable method
+     */
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new ServiceOnPlayerTouchAS(),this);
     }
 
+    /**
+     * @param p Cmd Executor
+     * @return Football
+     * @see #f(Player, double, double, double)
+     */
     protected Entity f(Player p) {
         return f(p,p.getLocation().getX(),p.getLocation().getY()+10,p.getLocation().getZ());
     }
+
+    /**
+     * @param p Cmd executor
+     * @param x Spawn location X
+     * @param y Spawn location Y
+     * @param z Spawn location Z
+     * @return Football
+     * @see #f(Player, double, double, double, String)
+     */
     protected Entity f(Player p,double x,double y,double z) {
         return f(p, x, y, z,"ASFUTBAL");
     }
-    protected Entity f(Player p,double x, double y, double z,String s){
-        return f(new Location(p.getWorld(),x,y,z),s);
+
+    /**
+     * @param p Cmd executor
+     * @param x Spawn location X
+     * @param y Spawn location Y
+     * @param z Spawn location Z
+     * @param n Football entity name. To use as a miniature, set n to something other than "ASFUTBAL".
+     * @return Football
+     * @see #f(Location, String)
+     */
+    protected Entity f(Player p,double x, double y, double z,String n){
+        return f(new Location(p.getWorld(),x,y,z),n);
     }
+
+    /**
+     * @param loc Location
+     * @param s Football entity name. To use as a miniature, set n to something other than "ASFUTBAL".
+     * @return Football
+     */
     private Entity f(Location loc,String s) {
         ArmorStand as = loc.getWorld().spawn(loc, ArmorStand.class);
         as.setCustomName(s);
